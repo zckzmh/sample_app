fc0baa334ee4f8da4071d2c5ef89a96408aee873
minghan <542518194@qq.com>
2015-03-17 15:57:20 +0800

    Finish singn in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 match '/signup',to: 'users#new',  via: 'get'
+match '/signin', to: 'sessions#new',via: 'get'
+match '/signout', to: 'sessions#destroy',via: 'delete'
 match '/home',to: 'static_pages#home',via: 'get'
 
 match '/help',to: 'static_pages#help',via: 'get'
